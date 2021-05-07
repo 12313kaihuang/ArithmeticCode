c00f7bdb0e89e4d9568ce395623a2174205be46f
huyu <2356048622@qq.com>
2021-05-07 19:45:57 +0800

    feat(leetcode):1486. 数组异或操作

---
+++ Leetcode/code/Solution1486_1.java
@@ -0,0 +1,84 @@
+package Leetcode.code;
+
+/**
+ * 1486. 数组异或操作
+ * <p>
+ * 给你两个整数，n 和 start 。
+ * <p>
+ * 数组 nums 定义为：nums[i] = start + 2*i（下标从 0 开始）且 n == nums.length 。
+ * <p>
+ * 请返回 nums 中所有元素按位异或（XOR）后得到的结果。
+ * <p>
+ * 示例 1：
+ * 输入：n = 5, start = 0
+ * 输出：8
+ * 解释：数组 nums 为 [0, 2, 4, 6, 8]，其中 (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8 。
+ * "^" 为按位异或 XOR 运算符。
+ * <p>
+ * 示例 2：
+ * 输入：n = 4, start = 3
+ * 输出：8
+ * 解释：数组 nums 为 [3, 5, 7, 9]，其中 (3 ^ 5 ^ 7 ^ 9) = 8.
+ * <p>
+ * 示例 3：
+ * 输入：n = 1, start = 7
+ * 输出：7
+ * <p>
+ * 示例 4：
+ * 输入：n = 10, start = 5
+ * 输出：2
+ * <p>
+ * 提示：
+ * 1 <= n <= 1000
+ * 0 <= start <= 1000
+ * n == nums.length
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/xor-operation-in-an-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution1486_1 {
+    public int xorOperation(int n, int start) {
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result = result ^ (start + 2 * i);
+        }
+        return result;
+    }
+
+    /**
+     * 官方题解
+     * 通过数学运算可以得出以下式子：
+     * x       x = 4k
+     * 1       x = 4k+1
+     * sumXor(x) =    x + 1   x = 4k+2
+     * 0       x = 4k+3
+     * 其中k是整数
+     * <p>
+     * 没太看懂，详见https://leetcode-cn.com/problems/xor-operation-in-an-array/solution/shu-zu-yi-huo-cao-zuo-by-leetcode-solution/
+     */
+    public int xorOperation2(int n, int start) {
+        int s = start >> 1, e = n & start & 1;
+        int ret = sumXor(s - 1) ^ sumXor(s + n - 1);
+        return ret << 1 | e;
+    }
+
+    public int sumXor(int x) {
+        if (x % 4 == 0) {
+            return x;
+        }
+        if (x % 4 == 1) {
+            return 1;
+        }
+        if (x % 4 == 2) {
+            return x + 1;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution1486_1().xorOperation(5, 0));
+        System.out.println(new Solution1486_1().xorOperation(4, 3));
+        System.out.println(new Solution1486_1().xorOperation(1, 7));
+    }
+}
