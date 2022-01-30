3644b9b82ee78bea34fcb0bfe7e35083f912c946
huyu <2356048622@qq.com>
2022-01-30 14:49:26 +0800

    feat(leetcode):77. 组合

--- Leetcode/README.md
+++ Leetcode/README.md
@@ -25,6 +25,7 @@
 * [62. 不同路径](./code/Solution62_1.java)
 * [71. 简化路径](./code/Solution71_1.java)
 * [74. 搜索二维矩阵](./code/Solution74_1.java)
+* [77. 组合](./code/Solution77_1.java)
 * [83. 删除排序链表中的重复元素](./code/Solution83_1.java)
 * [91. 解码方法](./code/Solution91_1.java)
 * [92. 反转链表 II](./code/Solution92_1.java)
---
+++ Leetcode/code/Solution77_1.java
@@ -0,0 +1,106 @@
+package Leetcode.code;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 77. 组合
+ * <p>
+ * 给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
+ * 你可以按 任何顺序 返回答案。
+ * <p>
+ * <p>
+ * 示例 1：
+ * 输入：n = 4, k = 2
+ * 输出：
+ * [
+ * [2,4],
+ * [3,4],
+ * [2,3],
+ * [1,2],
+ * [1,3],
+ * [1,4],
+ * ]
+ * <p>
+ * 示例 2：
+ * 输入：n = 1, k = 1
+ * 输出：[[1]]
+ * <p>
+ * 提示：
+ * 1 <= n <= 20
+ * 1 <= k <= n
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/combinations
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+@SuppressWarnings("unused")
+public class Solution77_1 {
+
+    List<Integer> temp = new ArrayList<Integer>();
+    List<List<Integer>> ans = new ArrayList<List<Integer>>();
+
+    /**
+     * 递归实现组合型枚举
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/combinations/solution/zu-he-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    public List<List<Integer>> combine(int n, int k) {
+        dfs(1, n, k);
+        return ans;
+    }
+
+    public void dfs(int cur, int n, int k) {
+        // 剪枝：temp 长度加上区间 [cur, n] 的长度小于 k，不可能构造出长度为 k 的 temp
+        if (temp.size() + (n - cur + 1) < k) return;
+        // 记录合法的答案
+        if (temp.size() == k) {
+            ans.add(new ArrayList<Integer>(temp));
+            return;
+        }
+        // 考虑选择当前位置
+        temp.add(cur);
+        dfs(cur + 1, n, k);
+        temp.remove(temp.size() - 1);
+        // 考虑不选择当前位置
+        dfs(cur + 1, n, k);
+    }
+
+    /**
+     * 非递归（字典序法）实现组合型枚举
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/combinations/solution/zu-he-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    public List<List<Integer>> combine2(int n, int k) {
+        List<Integer> temp = new ArrayList<Integer>();
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        // 初始化
+        // 将 temp 中 [0, k - 1] 每个位置 i 设置为 i + 1，即 [0, k - 1] 存 [1, k]
+        // 末尾加一位 n + 1 作为哨兵
+        for (int i = 1; i <= k; ++i) {
+            temp.add(i);
+        }
+        temp.add(n + 1);
+
+        int j = 0;
+        while (j < k) {
+            ans.add(new ArrayList<Integer>(temp.subList(0, k)));
+            j = 0;
+            // 寻找第一个 temp[j] + 1 != temp[j + 1] 的位置 t
+            // 我们需要把 [0, t - 1] 区间内的每个位置重置成 [1, t]
+            while (j < k && temp.get(j) + 1 == temp.get(j + 1)) {
+                temp.set(j, j + 1);
+                ++j;
+            }
+            // j 是第一个 temp[j] + 1 != temp[j + 1] 的位置
+            temp.set(j, temp.get(j) + 1);
+        }
+        return ans;
+    }
+}
